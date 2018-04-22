3771667804710adf339de2cb7bbe8e2a260d18f5
vivianluomin <273104241@qq.com>
2018-04-22 18:43:18 +0800

    开始写发布小说界面

---
+++ app/src/main/java/com/example/asus1/ourstory/Activities/PublishStroyActivity.java
@@ -0,0 +1,15 @@
+package com.example.asus1.ourstory.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.asus1.ourstory.R;
+
+public class PublishStroyActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_publish_stroy);
+    }
+}
